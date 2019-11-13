893d7784006bc6c80c3322f5b31148d15f37c4b7
pmatrix <pmatrixmedia@gmail.com>
2019-11-12 19:07:49 -0600

    added360world to aframe scene

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import 'aframe'
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <a-scene>
+        <a-sky src={require('./assets/360_world.jpg')} />
+      </a-scene>
+    );
+  }
 }
 
 export default App;
